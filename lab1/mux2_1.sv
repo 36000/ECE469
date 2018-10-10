4cafa280e37ec28281418d67eeb760906bca8a45
John A. Kruper <jk232@uw.edu>
2018-10-09 18:24:41 -0700

    mux

--- lab1/mux2_1.sv
+++ lab1/mux2_1.sv
@@ -5,12 +5,13 @@ module mux2_1(out, in0, in1, select);
 	output logic out;
 	logic s_bar, w1, w2;
 
-	not not1(s_bar, select);
+	not #50 not1(s_bar, select);
+	
+	
+	and #50 and1(w1, s_bar, in0);
+	and #50 and2(w2, select, in1);
 
-	and and1(w1, s_bar, in0);
-	and and2(w2, select, in1);
-
-	or or1(out, w1, w2);
+	or #50 or1(out, w1, w2);
 
 endmodule
 
