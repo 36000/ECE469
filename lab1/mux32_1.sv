0e4f8e9dc6e4fd0fcfe75eabe82ae972a677c698
John A. Kruper <jk232@uw.edu>
2018-10-09 17:03:21 -0700

    hol up

--- lab1/mux32_1.sv
+++ lab1/mux32_1.sv
@@ -7,9 +7,14 @@ module mux32_1(out, in, select);
 	logic [9:0] wires;
 
 	generate
-		for(i=0; i<4; i++) begin : lvl1
-			mux4_1 lvl1 ();
+		for(i=0; i<8; i++) begin : lvl1
+			mux4_1 lvl1 (wires[i], in[i*4], in[i*4+1], in[i*4+2], in[i*4+3], select[1], select[0]);
 		end
-	endgenerate 
+	endgenerate
+	
+	mux4_1(wires[8], wires[0], wires[1], wires[2], wires[3], select[3], select[2])
+	mux4_1(wires[9], wires[4], wires[5], wires[6], wires[7], select[3], select[2])
+	
+	mux2_1(out, wires[8], wires[9], select[4])
 
 endmodule
