790ee430ecfbdae47e17dff23bbc6dc1f1e03654
John A. Kruper <jk232@uw.edu>
2018-10-11 13:48:43 -0700

    reg1_bench

--- lab1/register_1.sv
+++ lab1/register_1.sv
@@ -1,16 +1,16 @@
-module register_1 (out, in, write, reset, clk);
+module register_1 (out, in, write_enable, reset, clk);
 
 	output logic out;
-	input logic in, write, reset, clk;
+	input logic in, write_enable, reset, clk;
 	logic w1;
 	
-	mux2_1 mux0 (w1, out, in, write);
+	mux2_1 mux0 (w1, out, in, write_enable);
 	D_FF dff0 (out, w1, reset, clk);
 	
 endmodule
 
 module register_1_testbench();
-		logic out, in, write, reset, clk
+		logic out, in, write_enable, reset, clk;
 		
 		parameter ClockDelay = 5000;
 		initial begin // Set up the clock
@@ -18,15 +18,16 @@ module register_1_testbench();
 			forever #(ClockDelay/2) clk <= ~clk;
 		end
 		
-		register_1 dut (.out, .in, .write, .reset, .clk);
+		register_1 dut (.out, .in, .write_enable, .reset, .clk);
 		
 		initial begin
-				in <=0, write<=0, reset<=1 @(posedge clk);
-				in <=1, write<=0, reset<=0 @(posedge clk);
-				in <=1, write<=1, reset<=0 @(posedge clk);
-				in <=0, write<=0, reset<=0 @(posedge clk);
-				in <=0, write<=0, reset<=1 @(posedge clk);
-				in <=1, write<=1, reset<=0 @(posedge clk);
-				in <=1, write<=1, reset<=1 @(posedge clk);
+				in <=0; write_enable<=0; reset<=1; @(negedge clk);
+				in <=1; write_enable<=0; reset<=0; @(negedge clk);
+				in <=1; write_enable<=1; reset<=0; @(negedge clk);
+				in <=0; write_enable<=0; reset<=0; @(negedge clk);
+				in <=0; write_enable<=0; reset<=1; @(negedge clk);
+				in <=1; write_enable<=1; reset<=0; @(negedge clk);
+				in <=1; write_enable<=1; reset<=1; @(negedge clk);
+				$stop;
 		end
 endmodule
--- lab1/simulation/modelsim/DE1_SoC_run_msim_rtl_verilog.do
+++ lab1/simulation/modelsim/DE1_SoC_run_msim_rtl_verilog.do
@@ -5,5 +5,15 @@ if {[file exists rtl_work]} {
 vlib rtl_work
 vmap work rtl_work
 
-vlog -sv -work work +incdir+C:/Users/micha/GitHub/ECE469/lab1 {C:/Users/micha/GitHub/ECE469/lab1/mux2_1.sv}
+vlog -sv -work work +incdir+U:/ECE469/lab1 {U:/ECE469/lab1/register_1.sv}
+vlog -sv -work work +incdir+U:/ECE469/lab1 {U:/ECE469/lab1/mux2_1.sv}
+vlog -sv -work work +incdir+U:/ECE469/lab1 {U:/ECE469/lab1/D_FF.sv}
 
+vlog -sv -work work +incdir+U:/ECE469/lab1 {U:/ECE469/lab1/register_1.sv}
+
+vsim -t 1ps -L altera_ver -L lpm_ver -L sgate_ver -L altera_mf_ver -L altera_lnsim_ver -L cyclonev_ver -L cyclonev_hssi_ver -L cyclonev_pcie_hip_ver -L rtl_work -L work -voptargs="+acc"  register_1_testbench
+
+add wave *
+view structure
+view signals
+run -all
