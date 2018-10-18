8c4b6db69c16cb4a1a9ec288f069904a6fe112d3
John A. Kruper <jk232@uw.edu>
2018-10-18 10:40:16 -0700

    semi-yeets

--- lab2/fulladder_1.sv
+++ lab2/fulladder_1.sv
@@ -5,12 +5,12 @@ module fulladder_1(A, B, C_in, C_out, S);
 	output logic C_out, S;
 	logic w1, w2, w3;
 
-	xor #50 xor1(w1, A, B)
-	xor #50 xor2(S, C_in, w1)
+	xor #50 xor1(w1, A, B);
+	xor #50 xor2(S, C_in, w1);
 	
-	and #50 and1(w2, C_in, w1)
-	and #50 and2(w3, A, B)
+	and #50 and1(w2, C_in, w1);
+	and #50 and2(w3, A, B);
 	
-	or #50 or1(C_out, w2, w3)
+	or #50 or1(C_out, w2, w3);
 
 endmodule
