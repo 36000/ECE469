1d373fa8dad41b749b46e94269cf741d72cbfcb5
John A. Kruper <jk232@uw.edu>
2018-10-20 10:56:29 -0700

    cleanup

--- lab2/alu.sv
+++ lab2/alu.sv
@@ -8,17 +8,18 @@ module alu(A, B, cntrl, result, negative, zero, overflow, carry_out);
 	output logic [63:0] result;
 	output logic negative, zero, overflow, carry_out;
 	
+	logic zeroBar;
 	logic subtract;
 	logic [63:0] carries_out;
 
 	// connect the 1 bit ALUs
 	genvar i;
 	ALU_1 firstALU (.A(A[0]), .B(B[0]), .C_in(subtract), .sub(subtract), .cntrl, .C_out(carries_out[0]), .S(result[0]));
-	generate begin
-		for (i = 1; i < 64; i++)
+	generate
+		for (i = 1; i < 64; i++) begin : ALUs
 			ALU_1 ALU1bit (.A(A[i]), .B(B[i]), .C_in(carries_out[i - 1]), .sub(subtract), .cntrl, .C_out(carries_out[i]), .S(result[i]));
-	end
-	
+		end
+	endgenerate
 	
 	// establish 'subtract' signal from control signal
 	and #50 subSignal (subtract, cntrl[1], cntrl[2]);
@@ -31,13 +32,7 @@ module alu(A, B, cntrl, result, negative, zero, overflow, carry_out);
 	xor #50 ovrflwSignal (overflow, carries_out[63], carries_out[62]);
 	
 	// establish 'zero' output signal
-	/*
-	logic [62:0] cascading_ORs;
-	genvar j;
-	generate begin
-		for (j = 0; j < 63; j++)
-			or #50 
-	end
-	*/
+	or_64 zeroSignalBar (result, zeroBar);
+	not #50 zeroSignal (zero, zeroBar);
 	
 endmodule
