9a5c214ecec2fd811deb7dde9ab5298b1551f756
MichaelHumphrey7 <michael@mkmc.net>
2018-11-07 14:13:03 -0800

    BUG FIXES
    it almost compiles now

--- lab3/controlblock.sv
+++ lab3/controlblock.sv
@@ -1,6 +1,6 @@
 
 
-parameter controlsigs_size = 14;
+// controlsigs should be 14 bits wide, because 14 control signals (shown directly below this comment)
 enum integer {Reg2Loc=0, ALUSrc=1, MemToReg=2, RegWrite=3, MemWrite=4, 
 			  BrTaken=5, UncondBr=6, ALUOp2=7, ALUOp1=8, ALUOp0=9,
 			  BigImm=10, Shift=11, SetFlag=12, isLTnotCBZ=13} signal;
@@ -13,22 +13,22 @@ enum logic[10:0] {ADDI=11'b1001000100Z, ADDS=11'b10101011000, AND=11'b1000101000
 // control block
 module controlblock(controlsigs, instr);
 	input logic [10:0] instr;
-	output logic [controlsigs_size-1:0] controlsigs;
+	output logic [14-1:0] controlsigs;
 
 	always_comb begin
 		casez(instr)
-			ADDI: controlsigs = controlsigs_size'b110100X010100X;
-			ADDS: controlsigs = controlsigs_size'b100100X010X01X;
-			AND:  controlsigs = controlsigs_size'b100100X100X00X;
-			B:    controlsigs = controlsigs_size'bXXX0011XXXXX0X;
-			B_LT: controlsigs = controlsigs_size'bXXX0010XXXXX01;
-			CBZ:  controlsigs = controlsigs_size'bXXX0010XXXXX00;
-			EOR:  controlsigs = controlsigs_size'b100100X110X00X;
-			LDUR: controlsigs = controlsigs_size'bX11100X010000X;
-			LSR:  controlsigs = controlsigs_size'bXX0100XXXXX10X;
-			STUR: controlsigs = controlsigs_size'b01X010X010000X;
-			SUBS: controlsigs = controlsigs_size'b100100X011X01X;
-			default: controlsigs = controlsigs_size'bXXX000XXXXXX0X;
+			ADDI: controlsigs = 14'b110100X010100X;
+			ADDS: controlsigs = 14'b100100X010X01X;
+			AND:  controlsigs = 14'b100100X100X00X;
+			B:    controlsigs = 14'bXXX0011XXXXX0X;
+			B_LT: controlsigs = 14'bXXX0010XXXXX01;
+			CBZ:  controlsigs = 14'bXXX0010XXXXX00;
+			EOR:  controlsigs = 14'b100100X110X00X;
+			LDUR: controlsigs = 14'bX11100X010000X;
+			LSR:  controlsigs = 14'bXX0100XXXXX10X;
+			STUR: controlsigs = 14'b01X010X010000X;
+			SUBS: controlsigs = 14'b100100X011X01X;
+			default: controlsigs = 14'bXXX000XXXXXX0X;
 		endcase
 	end
 	
